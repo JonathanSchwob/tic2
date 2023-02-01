8e4e12f4ee147ba20f4707c2a8e9fca9929187fc
Jonathan Schwob <jrschwob@gmail.com>
2023-01-31 19:45:03 -0800

    add checkWinner

--- src/Game.js
+++ src/Game.js
@@ -4,6 +4,7 @@ import Board from "./Board.js";
 function Game() {
   const [board, setBoard] = useState(Array(9).fill(null));
   const [xIsNext, setXIsNext] = useState("X");
+  const winner = checkWinner();
 
   function handlePlay(position) {
     if (board[position]) return;
@@ -19,27 +20,33 @@ function Game() {
     }
 
     setBoard(newBoard);
-    checkWinner();
   }
 
   function checkWinner() {
     const winStates = [
-      [1, 2, 3],
-      [4, 5, 6],
-      [7, 8, 9],
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
       [1, 4, 7],
       [2, 5, 8],
-      [3, 6, 9],
-      [1, 5, 9],
-      [3, 5, 7],
+      [0, 4, 8],
+      [2, 4, 6],
     ];
 
-    console.log(board, "ehllo");
+    for (let i = 0; i < winStates.length; i++) {
+      const [a, b, c] = winStates[i];
+      if (board[a] === board[b] && board[b] === board[c] && board[a] !== null) {
+        return "Winner is: " + board[a];
+      }
+    }
+
+    return "Next turn: " + xIsNext;
   }
 
   return (
     <>
-      <div className="status">Next Turn: {xIsNext}</div>
+      <div className="status">{winner}</div>
       <Board board={board} xIsNext={xIsNext} handlePlay={handlePlay} />
     </>
   );
